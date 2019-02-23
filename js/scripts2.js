7d8a3f9187a28810c652f878f4177e57946ea912
AugustineOchieng <gusochieng@gmail.com>
2019-02-23 18:02:46 +0300

    add link function

---
+++ js/scripts2.js
@@ -0,0 +1,9 @@
+//$(document).ready(function(){
+ // $("button").click(function(){
+   // $(".container").slideToggle(1000, function(){
+    //});
+  //});
+//});
+function myFunction() {
+  location.replace("file:///home/moringa/Documents/moringa-school-projects/quizboard/index2.html")
+}
