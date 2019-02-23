35dca3b5438881dc0e6150c5b64fa929a80f725c
AugustineOchieng <gusochieng@gmail.com>
2019-02-23 19:10:33 +0300

    add link to index2.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
 $(document).ready(function(){
   
   $("form,#input").submit(function(event){
-    //var qst1 = parseInt($("input:radio[name=question1]:checked").val());
-    //var qst2 = parseInt($("input:radio[name=question2]:checked").val());
-    //var qst3 = parseInt($("input:radio[name=question3]:checked").val());
-    //var qst4 = parseInt($("input:radio[name=question4]:checked").val());
+    var qst1 = parseInt($("input:radio[name=question1]:checked").val());
+    var qst2 = parseInt($("input:radio[name=question2]:checked").val());
+    var qst3 = parseInt($("input:radio[name=question3]:checked").val());
+    var qst4 = parseInt($("input:radio[name=question4]:checked").val());
 
-    //var total = qst1 + qst2 + qst3 + qst4;
-    //$("#result").text("You scored " + total + " %");
+    var total = qst1 + qst2 + qst3 + qst4;
+    $("#result").text("You scored " + total + " %");
 
-     //event.preventDefault()
-//
-  });
+     event.preventDefault()
+      });
 });
